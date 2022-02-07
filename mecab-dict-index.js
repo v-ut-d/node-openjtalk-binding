7e6c54e413a5c512232c8867be573bc3e8320970
femshima <49227365+femshima@users.noreply.github.com>
2022-02-07 15:01:24 +0900

    [add]Add interface to  mecab-dict-index

---
+++ mecab-dict-index.js
@@ -0,0 +1,10 @@
+const binary = require("@tignear/node-pre-gyp");
+const path = require('path');
+const meta = binary.meta(path.resolve(path.join(__dirname, './package.json')));
+const path_to_mecab_dict_index = path.resolve(path.join(meta.module_path, 'mecab-dict-index'));
+
+const spawn = require("child_process").spawn;
+const mecab = spawn(path_to_mecab_dict_index, process.argv.slice(2));
+process.stdin.pipe(mecab.stdin);
+mecab.stdout.pipe(process.stdout);
+mecab.stderr.pipe(process.stderr);
